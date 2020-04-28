ceef2a7785fc3670cda01e3d271e42569305282b
adamtclark <adam.tclark@gmail.com>
2020-04-28 16:51:05 +0200

    updated to run with new observation function

--- analyze_burgmer.R
+++ analyze_burgmer.R
@@ -138,17 +138,19 @@ if(FALSE) {
     #smap_out<-s_map(y, E=Euse, theta=tuse_edm, silent = TRUE, lib = libuse_y, save_smap_coefficients = TRUE)
     #smap_coefs<-smap_out$smap_coefficients[[1]]
     
-    pfout<-data.frame(rep=NA, Nest=rep(NA, length(y)), Nsd=NA)
+    pfout<-data.frame(rep=NA, Nest=rep(NA, length(y)), Nsd=NA, Nsmp=NA, Nsmp_noproc=NA)
     for(i in 1:nrow(libuse_y)) {
       ysegment<-y[libuse_y[i,1]:libuse_y[i,2]]
       smap_coefs_segment<-smap_coefs[libuse_y[i,1]:libuse_y[i,2],]
       
       tmp<-particleFilterLL(ysegment, pars, N=N, detfun = EDMfun0,
                             edmdat = list(E=Euse, theta=tuse_edm, smp_cf=smap_coefs_segment),
-                            dotraceback = TRUE)
+                            dotraceback = TRUE, fulltraceback = TRUE)
       pfout$Nest[libuse_y[i,1]:libuse_y[i,2]]<-tmp$Nest
       pfout$Nsd[libuse_y[i,1]:libuse_y[i,2]]<-tmp$Nsd
       pfout$rep[libuse_y[i,1]:libuse_y[i,2]]<-i
+      pfout$Nsmp[libuse_y[i,1]:libuse_y[i,2]]<-c(indexsort(tmp$fulltracemat, tmp$fulltraceindex, nsmp=1))
+      pfout$Nsmp_noproc[libuse_y[i,1]:libuse_y[i,2]]<-c(indexsort(tmp$fulltracemat_noproc, tmp$fulltraceindex, nsmp=1))
     }
     
     return(pfout)
@@ -166,19 +168,25 @@ if(FALSE) {
   smp_EDM<-(getSample(out_EDM, start=floor(niter/5)))
   simout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
   sdout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+  simout_smp<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+  simout_smp_noproc<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
   for(i in 1:nrow(smp_EDM)) {
     tmp<-particleFilterLL_piecewise(smp_EDM[i,], N=N)
     simout[,i]<-tmp$Nest
     sdout[,i]<-tmp$Nsd
+    simout_smp[,i]<-tmp$Nsmp
+    simout_smp_noproc[,i]<-tmp$Nsmp_noproc
     
     if(i/10 == floor(i/10)) {
       print(round(i/nrow(smp_EDM),3))
     }
   }
-  save.image("tmp2.rda", version = 2)
+  #save.image("tmp3.rda", version = 2)
+  #save.image("tmp2.rda", version = 2)
   #save.image("tmp.rda", version = 2)
 } else {
-  load("tmp2.rda")
+  load("tmp3.rda")
+  #load("tmp2.rda")
   #load("tmp.rda")
 }
 
@@ -191,22 +199,29 @@ m<-rbind(c(1,1,2,5),
          c(1,1,4,6),
          c(1,1,4,6))
 layout(m)
-ymax<-4.5
-plot(range(dat$time[yps]), c(0, ymax), xlab="time", ylab="abundance", type="n", xaxs="i", main="Corrected")
+ymax<-6
 fullerror<-FALSE
+tracedparticles<-TRUE
+
+plot(range(dat$time[yps]), c(0, ymax), xlab="time", ylab="abundance", type="n", xaxs="i", main="Corrected")
 
 for(i in 1:nrow(libuse_y)) {
   if(fullerror) {
-    tmpsmp<-matrix(nrow=length(libuse_y[i,1]:libuse_y[i,2]), ncol=ncol(simout))
     tmpsmp[]<-pmax(0, rnorm(n = length(simout[libuse_y[i,1]:libuse_y[i,2],]),
                   mean = simout[libuse_y[i,1]:libuse_y[i,2],],
                   sd = sdout[libuse_y[i,1]:libuse_y[i,2],]))
   } else {
-    tmpsmp<-simout[libuse_y[i,1]:libuse_y[i,2],]
+    if(tracedparticles) {
+      tmpsmp<-simout_smp[libuse_y[i,1]:libuse_y[i,2],]
+      psps<-which(is.na(tmpsmp[]))
+      tmpsmp[psps]<-simout[libuse_y[i,1]:libuse_y[i,2],][psps]
+    } else {
+      tmpsmp<-simout[libuse_y[i,1]:libuse_y[i,2],]
+    }
   }
-  tmp<-t(apply(tmpsmp, 1, function(x) quantile(x, probs = pnorm(c(-1,1)))))
+  tmp<-t(apply(tmpsmp, 1, function(x) quantile(x, probs = pnorm(c(-2:2)))))
   tm<-dat$time[yps][libuse_y[i,1]:libuse_y[i,2]]
-  polygon(c(tm, rev(tm)), c(tmp[,1], rev(tmp[,2])), col=adjustcolor(collst[i], alpha.f = 0.8))
+  polygon(c(tm, rev(tm)), c(tmp[,2], rev(tmp[,4])), col=adjustcolor(collst[i], alpha.f = 0.8))
   i<-i+1
 }
 abline(h=0, lty=3)
@@ -223,13 +238,124 @@ plot(range(dat$time[yps]), c(0, ymax), xlab="time", ylab="abundance", type="n", 
 for(i in 1:nrow(libuse_y)) {
   lines(dat$time[yps][libuse_y[i,1]:libuse_y[i,2]], y[libuse_y[i,1]:libuse_y[i,2]], col=adjustcolor(collst[i], alpha.f = 0.8))
 }
-tmp<-t(apply(simout, 1, function(x) quantile(x, probs = pnorm(c(-2,0,2)))))
 abline(h=0, lty=3)
 
-plot(tmp[,2], y, xlab="Corrected", ylab="Raw", col=adjustcolor(1, alpha.f = 0.2))
+if(fullerror) {
+  tmp<-matrix(nrow=nrow(simout), ncol=ncol(simout))
+  tmp[]<-rnorm(length(simout), simout, sdout)
+  tmp<-t(apply(tmp, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)))))
+} else {
+  if(tracedparticles) {
+    tmp<-t(apply(simout_smp, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)), na.rm=T)))
+  } else {
+    tmp<-t(apply(simout, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)))))
+  }
+}
+
+plot(tmp[,2], y, xlab="Corrected", ylab="Raw", col=adjustcolor(1, alpha.f = 0.2), xlim=c(0, ymax), ylim=c(0, ymax))
 segments(tmp[,1], y, tmp[,3], y, lend=2, col=adjustcolor(1, alpha.f = 0.2))
 obsest<-pmax(0.01, y*mean(exp(smp_EDM[,1])))
 segments(tmp[,2], y-obsest, tmp[,2], y+obsest, lend=2, col=adjustcolor(1, alpha.f = 0.2))
 abline(a=0, b=1, lty=2, lwd=2)
 abline(h=0, v=0, lty=3)
 
+
+
+
+
+
+#Use inverse process function
+#Make sure to use full set of outcomes to calculate p-values. 
+
+#Think about last time point...
+
+plot(c(0,1), c(0, 1), ylab="pmor[obs]", xlab="pmor[sim]", type="n")
+for(i in 1:nrow(libuse_y)) {
+  tmpsmp<-simout_smp_noproc[libuse_y[i,1]:libuse_y[i,2],]
+  tmpsmp_proc<-simout_smp[libuse_y[i,1]:libuse_y[i,2],]
+  
+  plot(rowMeans(tmpsmp_proc))
+  plot(rowMeans(simout[libuse_y[i,1]:libuse_y[i,2],]))
+  
+  tmp<-apply(tmpsmp, 1, function(x) mean(procfun0(c(mean(smp_EDM[,2]), mean(smp_EDM[,3])),x,inverse = TRUE)))
+  prod(1-tmp[!is.na(tmp)]) #pr of extinction
+  mean(tmpsmp_proc[17,]==0) #actual extinction
+  #need to think about effect of minsd?
+  #maybe to think about adding 2 obs params again?... Or reducing?
+  tmp<-(exp(mean(smp_EDM[,1]))*tmpsmp_proc[17,])
+  hist(tmp[tmp<0.1], breaks = 20)
+  hist(pmax(tmp[tmp<0.1], 0.01), breaks = 20)
+  
+  x<-0.1999774
+  pnorm(x, sqrt(exp(mean(smp_EDM[,2]))*x^exp(mean(smp_EDM[,3]))))
+  
+  std_tmp<-exp(mean(smp_EDM[,1]))*x
+  dnorm((0-x)/std_tmp,log=TRUE)-log(std_tmp)
+  pnorm(-0/(0.01))
+  
+  
+  
+  apply(tmpsmp_proc, 1, function(x) length(unique(x)))
+  
+  std_tmp<-sqrt(exp(rep(smp_EDM[,2], each=nrow(tmpsmp)))*tmpsmp^exp(
+    rep(smp_EDM[,3], each=nrow(tmpsmp))
+  ))
+  
+  tm<-dat$time[yps][libuse_y[i,1]:libuse_y[i,2]]
+  
+  if(FALSE) {
+    #full sets of particles
+    pr_died_cv<-pnorm(0, tmpsmp, std_tmp)
+    pr_died_cv[tmpsmp==0 | std_tmp==0]<-NA
+    
+    pr_died<-tmpsmp_proc[-1,]==0 & tmpsmp_proc[-nrow(tmpsmp_proc),]>0 & is.finite(tmpsmp_proc[-1,]) & is.finite(tmpsmp_proc[-nrow(tmpsmp_proc),])
+    pr_died[!(tmpsmp_proc[-nrow(tmpsmp_proc),]>0 & is.finite(tmpsmp_proc[-1,]) & is.finite(tmpsmp_proc[-nrow(tmpsmp_proc),]))]<-NA
+    
+    
+    cum_pr_died_cv<-1-apply(pr_died_cv, 2, function(x) {x[is.na(x)]<-0; cumprod(1-x)})
+    cum_pr_died<-1-apply(pr_died, 2, function(x) {x[is.na(x)]<-0; cumprod(1-x)})
+    
+    #BINOMIAL MODEL!!
+    plot(cum_pr_died_cv[-nrow(cum_pr_died_cv),], as.numeric(pr_died))
+    
+    plot(rowMeans(cum_pr_died_cv[-nrow(cum_pr_died_cv),]), rowMeans(cum_pr_died))
+  }
+  
+  if(FALSE) {
+    #plot states
+    tmp<-t(apply(tmpsmp, 1, function(x) quantile(x, probs = pnorm(c(-2:2)), na.rm=T)))
+    plot(tm, tmp[,3])
+    
+    tmp_proc<-t(apply(tmpsmp_proc, 1, function(x) quantile(x, probs = pnorm(c(-2:2)), na.rm=T)))
+    plot(tm, tmp_proc[,3])
+  }
+  if(FALSE) {
+    #plot CV
+    plot(tmpsmp, std_tmp)
+    abline(a=0, b=1, lty=2, col=2, lwd=2)
+    
+    plot(tmpsmp, std_tmp/tmpsmp)
+  }
+  
+  #probabilities
+  pr_died<-rowSums(tmpsmp_proc[-1,]==0 & tmpsmp_proc[-nrow(tmpsmp_proc),]>0 & is.finite(tmpsmp_proc[-1,]) & is.finite(tmpsmp_proc[-nrow(tmpsmp_proc),]))/
+    rowSums(tmpsmp_proc[-nrow(tmpsmp_proc),]>0 & is.finite(tmpsmp_proc[-1,]) & is.finite(tmpsmp_proc[-nrow(tmpsmp_proc),]))
+  
+  pr_died_cv<-pnorm(0, tmpsmp, std_tmp)
+  pr_died_cv[tmpsmp==0 | std_tmp==0]<-NA
+  pr_died_cv_mean<-apply(pr_died_cv, 1, function(x) mean(x[is.finite(x)]))
+  if(FALSE) {
+    plot(pr_died_cv_mean[-length(pr_died_cv_mean)], pr_died)
+    
+    plot(cumprod((1-pr_died_cv_mean[-length(pr_died_cv_mean)])[-c(1:3)]),
+         cumprod((1-pr_died)[-c(1:3)]))
+  }
+  points(1-cumprod((1-pr_died_cv_mean[-length(pr_died_cv_mean)])[-c(1:3)]),
+         1-cumprod((1-pr_died)[-c(1:3)]),
+         col=adjustcolor(collst[i], alpha.f = 0.8))
+  #cv_int<-t(apply(std_tmp/tmpsmp, 1, function(x) quantile(x, probs = pnorm(c(-2:2)), na.rm=T)))
+  #polygon(c(tm, rev(tm)), c(cv_int[,2], rev(cv_int[,4])), col=adjustcolor(collst[i], alpha.f = 0.8))
+  #lines(c(tm), c(cv_int[,3]), col=adjustcolor(collst[i], alpha.f = 0.8), lwd=1.5)
+  #i<-i+1
+}
+abline(h=c(0, 1), lty=3)
