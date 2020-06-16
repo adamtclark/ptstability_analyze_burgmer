d2262f5c241a342734b4da7856a00ba3c7a96026
adamtclark <adam.tclark@gmail.com>
2020-06-16 14:31:00 +0200

    updated to run only analysis

--- analyze_burgmer.R
+++ analyze_burgmer.R
@@ -164,177 +164,51 @@ particleFilterLL_piecewise<-function(param, N) {
 
 bayesianSetup_EDM <- createBayesianSetup(likelihood = likelihood_EDM_piecewise, prior = prior_edm)
 
-#only run if not yet saved
-#if(length(grep(paste(simname, ".rda", sep=""), dir("datout/")))==0) {
-if(TRUE) {
-  #run MCMC optimization
-  out_EDM <- runMCMC(bayesianSetup = bayesianSetup_EDM, settings = list(iterations=niter, consoleUpdates=200))
-  #plot(out_EDM, start = floor(niter/5))
-  #correlationPlot(out_EDM, start = floor(niter/5))
+#run MCMC optimization
+out_EDM <- runMCMC(bayesianSetup = bayesianSetup_EDM, settings = list(iterations=niter, consoleUpdates=200))
+#plot(out_EDM, start = floor(niter/5))
+#correlationPlot(out_EDM, start = floor(niter/5))
+
+#get trajectories
+smp_EDM<-(getSample(out_EDM, start=floor(niter/5)))
+simout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+sdout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+simout_smp<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+simout_smp_noproc<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
+for(i in 1:nrow(smp_EDM)) {
+  tmp<-particleFilterLL_piecewise(smp_EDM[i,], N=N)
+  simout[,i]<-tmp$Nest
+  sdout[,i]<-tmp$Nsd
+  simout_smp[,i]<-tmp$Nsmp
+  simout_smp_noproc[,i]<-tmp$Nsmp_noproc
   
-  #get trajectories
-  smp_EDM<-(getSample(out_EDM, start=floor(niter/5)))
-  simout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
-  sdout<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
-  simout_smp<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
-  simout_smp_noproc<-matrix(nrow=length(y), ncol=nrow(smp_EDM))
-  for(i in 1:nrow(smp_EDM)) {
-    tmp<-particleFilterLL_piecewise(smp_EDM[i,], N=N)
-    simout[,i]<-tmp$Nest
-    sdout[,i]<-tmp$Nsd
-    simout_smp[,i]<-tmp$Nsmp
-    simout_smp_noproc[,i]<-tmp$Nsmp_noproc
-    
-    if(i/10 == floor(i/10)) {
-      print(round(i/nrow(smp_EDM),3))
-    }
+  if(i/10 == floor(i/10)) {
+    print(round(i/nrow(smp_EDM),3))
   }
-  
-  # calculate pmor per timestep
-  xtedm<-simout_smp_noproc
-  stdedm<-sqrt(rep(exp(smp_EDM[,2]), each=nrow(xtedm))*
-                 xtedm^rep(exp(smp_EDM[,3]), each=nrow(xtedm)))
-  
-  pmedm_analy<-matrix(nrow=ncol(xtedm), ncol=nrow(libuse_y)+1)
-  
-  for(i in 1:ncol(xtedm)) {
-    for(j in 1:nrow(libuse_y)) {
-      ps<-which(!is.na(xtedm[,i]) & xtedm[,i]>0)
-      ps<-ps[ps%in%c(libuse_y[j,1]:libuse_y[j,2])]
-      if(length(ps)>0) {
-        pmedm_analy[i,j]<-sum(pnorm(0, xtedm[ps,i], stdedm[ps,i]))/length(ps)
-      } else {
-        pmedm_analy[i,j]<-0
-      }
-    }
-    
-    ps<-(!is.na(xtedm[,i]) & xtedm[,i]>0)
-    pmedm_analy[i,nrow(libuse_y)+1]<-sum(pnorm(0, xtedm[ps,i], stdedm[ps,i]))/sum(ps)
-  }
-  
-  
-  save(list = c("out_EDM", "smp_EDM", "simout", "sdout", "simout_smp", "simout_smp_noproc", "pmedm_analy"),
-       file = paste("datout/", simname, ".rda", sep=""), version = 2)
-} else {
-  load(paste("datout/", simname, ".rda", sep=""))
 }
 
-#plot outputs
-pdf(paste("plotout/", simname, ".pdf", sep=""), width=10, height=6, colormodel = "cmyk", useDingbats = FALSE)
-  par(mar=c(3.5,3.5,2,1), oma=c(1,1,0,0))
-  m<-rbind(c(1,1,5,2),
-           c(1,1,5,2),
-           c(1,1,6,3),
-           c(1,1,6,3),
-           c(1,1,7,4),
-           c(1,1,7,4))
-  layout(m)
-  ymax<-6
-  fullerror<-FALSE
-  tracedparticles<-TRUE
-  collst<-c(viridis(nrow(libuse_y)), "black")
-  
-  plot(range(dat$time[yps]), c(0, ymax), xlab="", ylab="", type="n", xaxs="i", main="Corrected")
-  
-  for(i in 1:nrow(libuse_y)) {
-    if(fullerror) {
-      tmpsmp[]<-pmax(0, rnorm(n = length(simout[libuse_y[i,1]:libuse_y[i,2],]),
-                    mean = simout[libuse_y[i,1]:libuse_y[i,2],],
-                    sd = sdout[libuse_y[i,1]:libuse_y[i,2],]))
+# calculate pmor per timestep
+xtedm<-simout_smp_noproc
+stdedm<-sqrt(rep(exp(smp_EDM[,2]), each=nrow(xtedm))*
+               xtedm^rep(exp(smp_EDM[,3]), each=nrow(xtedm)))
+
+pmedm_analy<-matrix(nrow=ncol(xtedm), ncol=nrow(libuse_y)+1)
+
+for(i in 1:ncol(xtedm)) {
+  for(j in 1:nrow(libuse_y)) {
+    ps<-which(!is.na(xtedm[,i]) & xtedm[,i]>0)
+    ps<-ps[ps%in%c(libuse_y[j,1]:libuse_y[j,2])]
+    if(length(ps)>0) {
+      pmedm_analy[i,j]<-sum(pnorm(0, xtedm[ps,i], stdedm[ps,i]))/length(ps)
     } else {
-      if(tracedparticles) {
-        tmpsmp<-simout_smp[libuse_y[i,1]:libuse_y[i,2],]
-        psps<-which(is.na(tmpsmp[]))
-        tmpsmp[psps]<-simout[libuse_y[i,1]:libuse_y[i,2],][psps]
-      } else {
-        tmpsmp<-simout[libuse_y[i,1]:libuse_y[i,2],]
-      }
-    }
-    tmp<-t(apply(tmpsmp, 1, function(x) quantile(x, probs = pnorm(c(-2:2)))))
-    tm<-dat$time[yps][libuse_y[i,1]:libuse_y[i,2]]
-    polygon(c(tm, rev(tm)), c(tmp[,2], rev(tmp[,4])), col=adjustcolor(collst[i], alpha.f = 0.8))
-    i<-i+1
-  }
-  abline(h=0, lty=3)
-  abline(v=(dat$time[yps][libuse_y[1,1]:libuse_y[1,2]])[Euse], lty=3)
-  mtext("Time (days)", side = 1, line = 2.6)
-  mtext("Std. Abundance", side = 2, line = 2.6)
-  
-  
-  hist(exp(smp_EDM[,1]), main="Obs. Error", xlab="", ylab="", breaks = 20, probability = TRUE)
-  abline(v=mean(exp(smp_EDM[,1])), lwd=2, col="blue")
-  hist(exp(smp_EDM[,2]), main="Proc. Noise, a", xlab="", ylab="", breaks = 20, probability = TRUE)
-  abline(v=mean(exp(smp_EDM[,2])), lwd=2, col="blue")
-  hist(exp(smp_EDM[,3]), main="Proc. Noise, b", xlab="", ylab="", breaks = 20, probability = TRUE)
-  abline(v=mean(exp(smp_EDM[,3])), lwd=2, col="blue")
-  mtext("Density", side = 2, outer=TRUE, padj = 63)
-  mtext("Estimate", side = 1, line = 2.6)
-  
-  
-  
-  plot(range(dat$time[yps]), c(0, ymax), xlab="", ylab="", type="n", xaxs="i", main="Raw")
-  for(i in 1:nrow(libuse_y)) {
-    lines(dat$time[yps][libuse_y[i,1]:libuse_y[i,2]], y[libuse_y[i,1]:libuse_y[i,2]], col=adjustcolor(collst[i], alpha.f = 0.8))
-  }
-  abline(h=0, lty=3)
-  mtext("Time (days)", side = 1, line = 2.6)
-  mtext("Std. Abundance", side = 2, line = 2.6)
-  
-  
-  if(fullerror) {
-    tmp<-matrix(nrow=nrow(simout), ncol=ncol(simout))
-    tmp[]<-rnorm(length(simout), simout, sdout)
-    tmp<-t(apply(tmp, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)))))
-  } else {
-    if(tracedparticles) {
-      tmp<-t(apply(simout_smp, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)), na.rm=T)))
-    } else {
-      tmp<-t(apply(simout, 1, function(x) quantile(x, probs = pnorm(c(-1,0,1)))))
+      pmedm_analy[i,j]<-0
     }
   }
   
-  plot(tmp[,2], y, xlab="", ylab="", col=adjustcolor(1, alpha.f = 0.2), xlim=c(0, ymax), ylim=c(0, ymax))
-  segments(tmp[,1], y, tmp[,3], y, lend=2, col=adjustcolor(1, alpha.f = 0.2))
-  obsest<-pmax(0.01, y*mean(exp(smp_EDM[,1])))
-  segments(tmp[,2], y-obsest, tmp[,2], y+obsest, lend=2, col=adjustcolor(1, alpha.f = 0.2))
-  abline(a=0, b=1, lty=2, lwd=2)
-  abline(h=0, v=0, lty=3)
-  mtext("Raw", side = 1, line = 2.6)
-  mtext("Corrected", side = 2, line = 2.6)
-  
-  
-  #transform into total pr-mor across experiment
-  pmedm_tot<-1-(1-pmedm_analy)^(mean(libuse_y[,2]-libuse_y[,1]))
-  
-  #plot outputs
-  dnsrng<-c(0, 1)
-  dns<-NULL
-  
-  for(i in 1:ncol(pmedm_tot)) {
-    dns[[i]]<-density(pmedm_tot[,i], from = dnsrng[1], to = dnsrng[2], bw = diff(dnsrng)/40)
-  }
-  
-  yrng<-c(0, max(sapply(dns, function(x) max(x$y))))
-  
-  plot(dnsrng, yrng, type="n", xlab="", ylab="")
-  collst2<-c(adjustcolor(viridis(ncol(pmedm_tot)-1), alpha.f = 0.6), "black")
-  lwdlst<-c(rep(1, ncol(pmedm_tot)-1), 2)
-  ltylst<-c(rep(1, ncol(pmedm_tot)-1), 1)
-  for(i in 1:ncol(pmedm_tot)) {
-    lines(dns[[i]]$x, dns[[i]]$y, col=collst2[i], lwd=lwdlst[i], lty=ltylst[i])
-  }
-  abline(h=0, v=c(0,1), lty=3)
-  mormu<-mean(pmedm_tot[,ncol(pmedm_tot)])
-  abline(v=mormu, lwd=2, lty=2)
-  
-  mtext("Pr[extinction]", side = 1, line = 2.6)
-  mtext("Density", side = 2, line = 2.6)
-dev.off()
+  ps<-(!is.na(xtedm[,i]) & xtedm[,i]>0)
+  pmedm_analy[i,nrow(libuse_y)+1]<-sum(pnorm(0, xtedm[ps,i], stdedm[ps,i]))/sum(ps)
+}
 
 
-sumdat<-data.frame(sp=trtsplst[commArg_ps,1], trt=trtuse,
-                   Euse, tuse,
-                   edm_r2, edm_mae, edm_mte,
-                   mormu, morsd=sd(pmedm_tot[,ncol(pmedm_tot)]))
-
-write.csv(sumdat, paste("datout/", simname, "_summary.csv", sep=""), row.names=F)
+save(list = c("out_EDM", "smp_EDM", "simout", "sdout", "simout_smp", "simout_smp_noproc", "pmedm_analy"),
+     file = paste("datout/", simname, ".rda", sep=""), version = 2)
